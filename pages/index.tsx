f66cd9e3c56d3227c22706b9ad54602360418657
Bruno Soares <bruno.soaest95@gmail.com>
2022-11-23 11:43:55 -0300

    teste revalidate

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,9 @@ export async function getStaticProps() {
   return {
     props: {
       jogos: post
-    }
+    },
+    revalidate: 3600/* apos 10 segundos, a proxima requicição ira atualizar o cache */
+    
   }
   
 }
