12a282c56357d0dc43f0123fc1a88313f69b963f
Bruno Soares <bruno.soaest95@gmail.com>
2022-11-23 11:24:40 -0300

    teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Home from '../screens/Home';
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const dadosDaAPI = await fetch('http://localhost:3000/api/jogos')
   .then((res) => {
     if(res.ok){
